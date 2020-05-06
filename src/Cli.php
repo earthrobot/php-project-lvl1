7465273f5845123ccb8366755c02c756615fefea
earthrobot <syley-20@yandex.ru>
2020-05-06 22:06:03 +0400

    Игра "Калькулятор"

--- src/Cli.php
+++ src/Cli.php
@@ -4,7 +4,6 @@ namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
-use function cli\choose;
 
 function run()
 {
@@ -16,7 +15,7 @@ function run()
 function game()
 {
     line('Welcome to the Brain Game!');
-    line('Answer "yes" if the numder is even, otherwise answer "no".');
+    line('Answer "yes" if the number is even, otherwise answer "no".');
     $name = prompt('May I have your name?');
     line("Hello, %s!\n", $name);
     for ($i = 0; $i < 3; $i++) {
@@ -32,6 +31,35 @@ function game()
             $result = 'Correct!';
         } else {
             $result = $answer . " is wrong answer ;). Correct answer was " . $even;
+            line($result);
+            return;
+        }
+        line($result);
+    }
+    line("Congratulations, %s!\n", $name);
+}
+
+function calc()
+{
+    line('Welcome to the Brain Game!');
+    line('What is the result of expression?');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!\n", $name);
+    for ($i = 0; $i < 3; $i++) {
+        $num1 = rand(1, 30);
+        $num2 = rand(1, 30);
+        $res1 = array('+', $num1 + $num2);
+        $res2 = array('-', $num1 - $num2);
+        $res3 = array('*', $num1 * $num2);
+        $operArr = array($res1, $res2, $res3);
+        $ind = rand(0, 2);
+        $oper = $operArr[$ind];
+        line('Question: ' . $num1 . ' ' . $oper[0] . ' ' . $num2);
+        $answer = prompt('Your answer');
+        if ($answer == $oper[1]) {
+            $result = 'Correct!';
+        } else {
+            $result = $answer . " is wrong answer ;). Correct answer was " . $oper[1];
             line($result);
             return;
         }
